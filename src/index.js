f3d970d1b76ae8d2ae42770f7be38b87eb644b96
setter-getter <dimkinyashchik@yandex.ru>
2023-02-03 22:54:23 +0400

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    bracketsConfig = bracketsConfig.map(el => el.join(''));
+
+    while (bracketsConfig.length) {
+      let counter = 0;
+      bracketsConfig.forEach(el => {
+        if (str.indexOf(el) != -1) {
+          str = str.replace(el, '');
+        } else {
+         counter++;
+        }
+      });
+
+      if (counter === bracketsConfig.length) {
+        bracketsConfig.length = 0;
+      }
+    }
+    return str.length === 0 ? true : false;
+  }
+
